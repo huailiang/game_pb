6ba1fd9b7ac057e8f41782e90fd1ae7d746efdde
huailiang.peng <penghuailiang@126.com>
2018-06-25 20:37:53 +0800

    build android shell script

--- Android/build_android.sh
+++ Android/build_android.sh
@@ -4,28 +4,28 @@ path=/Users/huailiang.peng/Documents/unity/game_pb
 
 cd ${path}
 
-rm -rf Android/jni/google/
-rm -rf Android/jni/interface/
+# rm -rf Android/jni/google/
+# rm -rf Android/jni/interface/
 
-cp -rf ptotobuf-lib/google Android/jni/
-cp -rf ptotobuf-lib/interface Android/jni/
+# cp -rf ptotobuf-lib/google Android/jni/
+# cp -rf ptotobuf-lib/interface Android/jni/
 
 
-cd Android/jni
+# cd Android/jni
 
-echo "start clean last so files"
+# echo "start clean last so files"
 
-ndk-build clean 
+# ndk-build clean 
 
-ndk-build
+# ndk-build
 
-echo "make new so success"
+# echo "make new so success"
 
 cd ${path}
 
 cp -f Android/libs/armeabi-v7a/libprotobuf-lib.so UnityEnv/Assets/Plugins/Android/libs/armeabi-v7a/libprotobuf-lib.so
 
-cp -f Android/libs/x86/libprotobuf-lib.so UnityEnv/Assets/Plugins/Android/libs/x86/libprotobuf-lib.so.so
+cp -f Android/libs/x86/libprotobuf-lib.so UnityEnv/Assets/Plugins/Android/libs/x86/libprotobuf-lib.so
 
 echo "copy so success, bye!"
 
